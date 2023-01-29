dc3865845ae6ea391557337508ca6f4d8121a8d3
Damien O'Hara <damien.j.ohara@gmail.com>
2023-01-29 15:12:05 +0800

    failing test of behavior

---
+++ markout/src/test/kotlin/ApplyModeTests.kt
@@ -0,0 +1,36 @@
+import io.koalaql.markout.markout
+import org.junit.Test
+import kotlin.io.path.Path
+import kotlin.io.path.deleteIfExists
+import kotlin.io.path.readText
+import kotlin.io.path.writeText
+import kotlin.test.assertEquals
+
+class ApplyModeTests {
+    @Test
+    fun `untracked files are never overwritten`() {
+        val untrackedContents = "test generated file"
+
+        val rootDir = Path("./test-data/untracked")
+            .apply {
+                resolve("untracked.txt").writeText(untrackedContents)
+                resolve(".markout").deleteIfExists()
+            }
+
+        repeat(3) { ix ->
+            try {
+                markout(rootDir) {
+                    file("untracked.txt", "changed contents")
+                }
+            } catch (ex: IllegalStateException) {
+                assertEquals("test-data/untracked/untracked.txt already exists", ex.message)
+            }
+
+            assertEquals(
+                untrackedContents,
+                rootDir.resolve("untracked.txt").readText(),
+                message = "run #${ix+1}:"
+            )
+        }
+    }
+}
