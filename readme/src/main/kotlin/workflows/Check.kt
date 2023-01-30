e4605ab94810ebd8600cbda07c8d7ef197e1f838
Damien O'Hara <damien.j.ohara@gmail.com>
2023-01-30 14:30:00 +0800

    check workflow is now generated

--- readme/src/main/kotlin/workflows/Check.kt
+++ readme/src/main/kotlin/workflows/Check.kt
@@ -2,12 +2,24 @@ package workflows
 
 import io.koalaql.markout.Markout
 import io.koalaql.markout.workflow
+import it.krzeminski.githubactions.actions.actions.CheckoutV3
+import it.krzeminski.githubactions.actions.actions.SetupJavaV3
+import it.krzeminski.githubactions.actions.gradle.WrapperValidationActionV1
+import it.krzeminski.githubactions.domain.RunnerType
 import it.krzeminski.githubactions.domain.triggers.PullRequest
 import it.krzeminski.githubactions.domain.triggers.Push
 
-fun Markout.checkWorkflow() = workflow("check2",
+fun Markout.checkWorkflow() = workflow("check",
     name = "Build and check",
     on = listOf(Push(), PullRequest()),
 ) {
-
+    job("build", runsOn = RunnerType.UbuntuLatest) {
+        uses(CheckoutV3())
+        uses(WrapperValidationActionV1())
+        uses(SetupJavaV3(
+            javaVersion = "19",
+            distribution = SetupJavaV3.Distribution.Temurin
+        ))
+        run("./gradlew check")
+    }
 }
